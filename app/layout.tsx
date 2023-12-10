b0525f16e5a561a25894128093a8527737b0ef02
WONHO22 <dnjsgh2122@gmail.com>
2023-12-11 02:23:53 +0900

    [Chore] hot-toast 라이브러리 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Pretendard } from "@/styles/font";
 import "@/styles/globals.css";
 import Header from "@/components/header/Header";
 import Navber from "@/components/navbar/Navbar";
+import { Toaster } from "react-hot-toast";
 
 interface Metadata {
   title: string;
@@ -23,6 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={Pretendard.className}>
       <body className="flex flex-col w-full h-full min-w-[1400px] max-w-[1920px]">
+        <Toaster
+          containerStyle={{
+            fontSize: "0.875rem",
+          }}
+        />
         <Header />
         <div className="flex">
           <Navber />
