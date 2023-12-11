b3fb0146af1c9c5c04b77fd2576007ec7ba3a9f0
WONHO22 <dnjsgh2122@gmail.com>
2023-12-11 20:02:55 +0900

    [Feat] 1. notebooks, notebooks-detail 페이지 로딩상태 구현

--- app/notebooks/layout.tsx
+++ app/notebooks/layout.tsx
@@ -4,6 +4,8 @@ import dynamic from "next/dynamic";
 import { useState, useEffect } from "react";
 import { Editor } from "@/components/editor/Editor";
 import EmptyPage from "./_components/EmptyPage";
+import Image from "next/image";
+import loadingGif from "@/public/assets/loading.gif";
 
 const NoteList = dynamic(() => import("@/components/common/NoteList"), {
   ssr: false,
@@ -11,24 +13,37 @@ const NoteList = dynamic(() => import("@/components/common/NoteList"), {
 
 const Notebooks: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   const { notebooks } = useNotebookStore();
-  const [isEmptyPage, setIsEmptyPage] = useState<Boolean>(true);
+  const [isLoading, setIsLoading] = useState<Boolean>(true);
+  const [isEmptyPage, setIsEmptyPage] = useState<Boolean>(false);
 
   useEffect(() => {
+    // notebooks가 비동기적으로 불러와질 때 로딩 상태 표시
+    setIsLoading(true);
+
+    // notebooks가 로드된 후에 로딩 상태 갱신
     if (notebooks.length >= 1) {
       setIsEmptyPage(false);
+      setIsLoading(false);
     } else {
       setIsEmptyPage(true);
+      setIsLoading(false);
     }
   }, [notebooks]);
 
   return (
     <div className="flex w-full">
-      {isEmptyPage ? (
+      {isLoading ? (
+        // 로딩 중인 경우 로딩 UI 표시
+        <div className="flex w-screen h-screen justify-center items-center">
+          <Image src={loadingGif} width={100} height={100} alt="loading gif" />
+        </div>
+      ) : isEmptyPage ? (
+        // 로딩이 완료되었지만 notebooks가 비어있는 경우
         <EmptyPage />
       ) : (
+        // notebooks가 정상적으로 불러와진 경우
         <>
           {/* <NoteList data={notebooks} /> */}
-
           {children}
         </>
       )}
